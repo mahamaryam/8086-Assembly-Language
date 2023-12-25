5bf1e68a6a3c209011e8726405a567552cc5ffc6
Maha Maryam <119899393+mahamaryam@users.noreply.github.com>
2023-12-25 14:11:20 +0500

    Create 11th_april_lab2.asm

---
+++ 11th_april_lab2.asm
@@ -0,0 +1,120 @@
+.model small
+.stack
+.data 
+var db 0  
+capital db 'A'
+small db 'a'
+table db 00h,01h,02h,03h,04h,05h,06h,07h,08h,09h,10h,11h
+      db 12h, 13h,14h,15h,16h,17h,18h,19h,20h,21h,22h,23h
+      db 24h,25h,26h
+       
+number db 0
+ reverse db 26 
+.code
+main proc
+        
+       ;mov ax,@data
+   ;    mov ds,ax
+    ;   mov bx,offset table  
+     ;  mov dx,offset var
+      ; mov al,'3'
+      ; sub al,48
+      ; xlat
+       ;mov var,al
+;;       shr al,4
+  ;     mov dl,al
+   ;    add dl,48
+    ;   mov ah,2
+     ;  int 21h
+      ; mov al,var
+;       shl al,4
+ ;      shr al,4
+  ;     mov dl,al
+   ;    add dl,48
+    ;   mov ah,2
+     ;  int 21h    
+     
+        mov ax,@data
+        mov ds,ax
+        mov bx,offset table
+        mov dx,offset capital
+        mov dx,offset small
+        mov dx,offset number
+        mov dx,offset reverse
+        mov dx,offset var
+        
+        mov cx,0
+        print:
+        mov dl,capital
+        mov ah,2
+        int 21h
+       
+        mov dl,32
+        mov ah,2
+        int 21h
+        int 21h
+        
+        mov dl,small
+        mov ah,2
+        int 21h
+        mov dl,32
+        mov ah,2
+        int 21h
+        int 21h
+        
+        mov ax,0
+        mov al,number
+        xlat
+       mov var,al
+       shr al,4
+       mov dl,al
+       add dl,48
+       mov ah,2
+       int 21h
+       mov al,var
+       shl al,4
+       shr al,4
+       mov dl,al
+       add dl,48
+       mov ah,2
+       int 21h 
+       
+       mov dl,32
+       mov ah,2
+       int 21h
+       int 21h
+       
+       mov ax,0
+       mov al,reverse
+       xlat
+       mov var,al
+       shr al,4
+       mov dl,al
+       add dl,48
+       mov ah,2
+       int 21h
+       mov al,var
+       shl al,4
+       shr al,4
+       mov dl,al
+       add dl,48
+       mov ah,2
+       int 21h   
+       mov dl,13
+       mov ah,2
+       int 21h
+       mov dl,10
+       mov ah,2
+       int 21h
+       
+       inc capital
+       inc small
+       dec reverse
+       inc number
+       loop print
+       
+        
+        
+    
+    main endp
+end main
