5844ede03762d189850bf1d7a8735ce1082a01c1
Maha Maryam <119899393+mahamaryam@users.noreply.github.com>
2023-12-25 14:12:05 +0500

    Create leapYear.asm

---
+++ leapYear.asm
@@ -0,0 +1,43 @@
+.model small
+.stack
+.data   
+msg db 0ah,0Dh,"Enter the Year: $"
+number db 5,0,5,"$"
+leap db 10,13,"Yes, it is a leap year!$"
+noleap db 10,13,"No, it is not a leap year$"
+.code
+main proc
+    
+      mov ax,@data
+      mov ds,ax
+      lea dx,msg
+      mov ah,9
+      int 21h
+      lea dx,number
+      mov ah,0ah
+      int 21h 
+      lea bx,number+4
+      mov ah,[bx]
+      mov al,[bx+1]     
+      aad            
+      ;ax has dividend
+      mov bl,4
+      div bl
+      cmp ah,0
+      je isleap
+      mov dx,offset noleap
+      mov ah,9
+      int 21h
+      jmp end
+      
+      isleap:
+      mov dx,offset leap
+      mov ah,9
+      int 21h
+      end:
+      mov ah,4ch
+      int 21h 
+      
+    
+    main endp
+end main
